ec38da001a0dcdc4b526128c38228a069a1b9d8b
Dave Trabka <dtrabka@gmail.com>
2018-06-08 14:31:51 -0400

    feat(stripe): stripe checkout now has access to cart items total price

--- client/components/stripe/CheckoutForm.js
+++ client/components/stripe/CheckoutForm.js
@@ -1,30 +1,31 @@
-import React from "react";
-import { injectStripe } from "react-stripe-elements";
-import CardSection from "./CardSection";
-import { connect } from 'react-redux';
-import axios from 'axios';
-import history from '../../history';
+import React from 'react'
+import { injectStripe } from 'react-stripe-elements'
+import CardSection from './CardSection'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import history from '../../history'
 
 class CheckoutForm extends React.Component {
-
-  handleSubmit = (ev) => {
-    ev.preventDefault();
-
-    this.props.stripe.createToken({ name: "Jenny Rosen" })
+  handleSubmit = ev => {
+    ev.preventDefault()
+    console.log('DID MY TOTAL MAKE IT?', this.props.totalPrice)
+    this.props.stripe
+      .createToken({ name: 'Jenny Rosen' })
       .then(({ token }) => {
-        return axios.post('/api/checkout', {token: token.id});
-      }).then((response) => {
-        if(response.status === 200){
+        return axios.post('/api/checkout', { token: token.id })
+      })
+      .then(response => {
+        if (response.status === 200) {
           history.push('/receipt')
-          // ! TODO: Create new page that will have thank you receipt and proper information 
+          // ! TODO: Create new page that will have thank you receipt and proper information
+          // TODO: Add to order model here
           // ! ADD button to go back to Home or Product page
         }
-      });
-  };
+      })
+  }
 
   render() {
-
-    console.log('payment method props', this.props.state);
+    console.log('payment method props', this.props.state)
     return (
       <div className="container container__sign-in-form white z-depth-2 animated fadeIn">
         <div id="payment-form" className="col s12">
@@ -33,14 +34,29 @@ class CheckoutForm extends React.Component {
               <h4 className="teal-text">Card Payment</h4>
               <div className="row">
                 <div className="input-field col s6">
-                  <input id="first_name" type="text" name="firstName" placeholder="First Name" />
+                  <input
+                    id="first_name"
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"
+                  />
                 </div>
                 <div className="input-field col s6">
-                  <input id="last_name" type="text" name="lastName" placeholder="Last Name" />
+                  <input
+                    id="last_name"
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                  />
                 </div>
                 <div className="row">
                   <div className="input-field col s12">
-                    <input id="homeAddress" type="text" name="homeAddress" placeholder="Address" />
+                    <input
+                      id="homeAddress"
+                      type="text"
+                      name="homeAddress"
+                      placeholder="Address"
+                    />
                   </div>
                 </div>
                 <div className="row">
@@ -49,23 +65,26 @@ class CheckoutForm extends React.Component {
                   </div>
                 </div>
                 <center>
-                  <button className="btn waves-effect waves-light teal" type="submit" name="action">
+                  <button
+                    className="btn waves-effect waves-light teal"
+                    type="submit"
+                    name="action">
                     Confirm order
                   </button>
                 </center>
               </div>
-              </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    )
   }
 }
 
-const mapState = (state) => {
+const mapState = state => {
   return {
-    state
+    state,
   }
 }
 
-export default injectStripe(connect(mapState)(CheckoutForm));
+export default injectStripe(connect(mapState)(CheckoutForm))
