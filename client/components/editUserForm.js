a88168de7f7d1e9117d8e2ba51b11b6d8ba85d89
Scott Soffel <ssoffel@hotmail.com>
2018-06-11 12:17:49 -0400

    updated UserEdit form to prepopulate

--- client/components/editUserForm.js
+++ client/components/editUserForm.js
@@ -5,10 +5,27 @@ import { connect } from 'react-redux';
 import { updateUser } from '../store/user';
 
 class EditUser extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+     firstName: ""
+    };
+  }
+
+  componentDidMount() {
+      this.setState({
+        firstName: this.props.user.firstName,
+        lastName: this.props.user.lastName,
+        homeAddress: this.props.user.homeAddress,
+        email: this.props.user.email
+      });
+
+  }
   handleSubmit = (evt) => {
     evt.preventDefault();
     const NU = {
-        id: this.props.user.id,
+      id: this.props.user.id,
       firstName: evt.target.firstName.value,
       lastName: evt.target.lastName.value,
       homeAddress: evt.target.homeAddress.value,
@@ -17,7 +34,18 @@ class EditUser extends Component {
     };
     this.props.updateUser(NU);
   };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
+    if (!this.props.user.firstName) {
+      console.log("Loading...");
+      return <h1>Loading...</h1>;
+    }
+
+    console.log("Userinfo", this.props.user)
     return (
       <div className="container container__sign-in-form white z-depth-2">
         <div id="register" className="col s12">
@@ -26,11 +54,13 @@ class EditUser extends Component {
               <h4 className="teal-text">Update</h4>
               <div className="row">
                 <div className="input-field col s6">
+
                   <input
                     id="first_name"
                     type="text"
                     name="firstName"
-                    placeholder="First Name"
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
                   />
                 </div>
                 <div className="input-field col s6">
@@ -38,7 +68,8 @@ class EditUser extends Component {
                     id="last_name"
                     type="text"
                     name="lastName"
-                    placeholder="Last Name"
+                    value={this.state.lastName}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -48,7 +79,8 @@ class EditUser extends Component {
                     id="homeAddress"
                     type="text"
                     name="homeAddress"
-                    placeholder="Address"
+                    value={this.state.homeAddress}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -58,7 +90,8 @@ class EditUser extends Component {
                     id="email"
                     type="email"
                     name="email"
-                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
