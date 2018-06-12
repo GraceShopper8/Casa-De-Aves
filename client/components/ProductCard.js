37a10c11f36544f5649f275e392f6b1929835f69
Dave Trabka <dtrabka@gmail.com>
2018-06-12 17:09:19 -0400

    feat(product-list): formatting

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,31 +1,31 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { me } from '../store';
-import { addedToCart } from '../store/cart';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { me } from '../store'
+import { addedToCart } from '../store/cart'
 
 class ProductCard extends Component {
   constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
+    super()
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(itemID) {
-    this.props.addedToCart(itemID);
+    this.props.addedToCart(itemID)
     setTimeout(() => {
-      let storage = window.localStorage;
-      const cartItems = this.props.items;
-      const str = JSON.stringify(cartItems);
-      storage.setItem('cart', str);
-    }, 50);
+      let storage = window.localStorage
+      const cartItems = this.props.items
+      const str = JSON.stringify(cartItems)
+      storage.setItem('cart', str)
+    }, 50)
   }
 
   render() {
-    const product = this.props.product;
+    const product = this.props.product
     return (
       <li className="side-by-side">
-        <div className="row">
+        <div className="row center">
           <div className="col s12 m6">
             <div className="card">
               <div className="card-image">
@@ -33,14 +33,18 @@ class ProductCard extends Component {
                   <img src={`/img/${product.imgUrl}`} />
                 </Link>
                 <a className="btn-floating btn-large halfway-fab waves-effect waves-light teal">
-                  <i className="material-icons" onClick={() => this.handleClick(product.id)}>
+                  <i
+                    className="material-icons"
+                    onClick={() => this.handleClick(product.id)}>
                     add_shopping_cart
                   </i>
                 </a>
               </div>
               <div className="card-content">
                 <Link to={`/products/${product.id}`}>
-                  <h5 className="black-text custom__text-size">{product.name}</h5>
+                  <h5 className="black-text custom__text-size">
+                    {product.name}
+                  </h5>
                 </Link>
                 <p>${product.price}</p>
               </div>
@@ -51,7 +55,7 @@ class ProductCard extends Component {
           </div>
         </div>
       </li>
-    );
+    )
   }
 }
 
@@ -59,18 +63,18 @@ const mapState = state => {
   return {
     items: state.cart.items,
     user: state.user,
-    isLoggedIn: !!state.user.id
-  };
-};
+    isLoggedIn: !!state.user.id,
+  }
+}
 
 const mapDispatch = dispatch => {
   return {
     loadInitialData: () => dispatch(me()),
-    addedToCart: id => dispatch(addedToCart(id))
-  };
-};
+    addedToCart: id => dispatch(addedToCart(id)),
+  }
+}
 
 export default connect(
   mapState,
   mapDispatch
-)(ProductCard);
+)(ProductCard)
