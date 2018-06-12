61595a3584eb8705b164e24b6f6e35af420efaae
Scott Soffel <ssoffel@hotmail.com>
2018-06-12 12:53:38 -0400

    feat(completed API test specs for api routes, initialized onClick handler in CompDiDMount for product filter)

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -8,10 +8,14 @@ import { getAllProducts, filterProducts } from '../store/product'
 class ProductList extends Component {
   async componentDidMount() {
     await this.props.getAllProducts()
+    const dropDown = document.querySelector(".dropdown-trigger");
+    M.Dropdown.init(dropDown);
   }
 
   handleDropDown = async(event) => {
+
     const category = event.target.name
+
     if (category === 'All'){
        await this.props.getAllProducts()
     } else {
@@ -31,8 +35,9 @@ class ProductList extends Component {
           <h2 className="float-header teal-text text-darken-3 center">
             All Products
           </h2>
-          <a className='dropdown-trigger btn large' href='#' data-target='dropdown1'>Filter
-          <i className="material-icons right">arrow_drop_down</i></a>
+
+          <a className='dropdown-trigger btn large'   data-target='dropdown1'>Filter
+          <i  className="material-icons right">arrow_drop_down</i></a>
 
           <ul id='dropdown1' onClick={this.handleDropDown} className='dropdown-content'>
             <li><a name="StarterHome">Starter Home</a></li>
@@ -43,6 +48,7 @@ class ProductList extends Component {
             <li className="divider" tabIndex="-1"></li>
             <li><a name="All">All</a></li>
           </ul>
+
           <ul className="products-box animated fadeIn">
             {productsList.map(product => (
               <ProductCard product={product} key={product.id} />
